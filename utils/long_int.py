20c3e189423316a3a7a113aa750fe307f3aec2d2
chocojoX <joseph.budin@polytechnique.edu>
2018-01-05 19:17:00 +0100

    Slight modification

--- utils/long_int.py
+++ utils/long_int.py
@@ -6,6 +6,7 @@ class LongInt(object):
         self.number = [0]
         self.add(number)
 
+
     def add(self, x):
         assert type(x)==int
         assert x>=0
@@ -34,34 +35,34 @@ class LongInt(object):
                 else:
                     self.number[i+1] = self.number[i+1]+1
 
+
     def multiply(self, x):
         assert type(x)==int
         assert x>=0
         self.number = [b*x for b in self.number]
 
         for j in range(len(self.number)):
-            k = self.basis; i=1
-            while int(self.number[j]/k)>0:
-                k = k*self.basis
-                i+=1
-            k = k/self.basis; i-=1
-            while i>0:
-                to_move = int(self.number[j]/k)
-                self.number[j] = self.number[j] - to_move*k
-                if i+j<len(self.number):
-                    self.number[j+i] = self.number[j+i] + to_move
-                else:
-                    self.number.append(to_move)
-                k = k/self.basis
-                i = i-1
+            to_move = int(self.number[j]/self.basis)
+            self.number[j] = self.number[j] - to_move*self.basis
+            if 1+j<len(self.number):
+                self.number[j+1] = self.number[j+1] + to_move
+            else:
+                self.number.append(to_move)
 
         for i, b in enumerate(self.number):
             if b>self.basis:
-                self.number[i] = b-self.basis
+                to_move = int(b/self.basis)
+                self.number[i] = b-to_move*self.basis
                 if len(self.number)==i+1:
-                    self.number.append(1)
+                    self.number.append(to_move)
                 else:
-                    self.number[i+1] = self.number[i+1]+1
+                    self.number[i+1] = self.number[i+1]+to_move
+
+        while self.number[-1]>=self.basis:
+            b = self.number[-1]
+            to_move = int(b/self.basis)
+            self.number[-1] = b-to_move*self.basis
+            self.number.append(to_move)
 
 
 
